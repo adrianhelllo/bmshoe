f1aedd1109c5c1658b103a1f429078aa3636f02a
adrianhelllo <hetessyadrian@gmail.com>
2026-04-26 13:36:42 +0200

    ensure file can be opened

--- bmshoe.c
+++ bmshoe.c
@@ -7,5 +7,15 @@ int main(int argc, char** argv)
     if (argc != 2)
     {
         printf("Correct usage: ./bmshoe file.bmp\n");
+        return 1;
     }
+
+    FILE* f = fopen(argv[1], "r");
+    if (f == NULL)
+    {
+        printf("Could not open %s.", argv[1]);
+        return 1;
+    }
+    
+    printf("%p", f);
 }
