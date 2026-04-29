4ae0d20bf45666f9338f3c4fe32a17e1e5d92e96
adrianhelllo <hetessyadrian@gmail.com>
2026-04-29 19:56:38 +0200

    update main file

--- bmshoe.c
+++ bmshoe.c
@@ -20,25 +20,4 @@ int main(int argc, char** argv)
         printf("Could not open %s.", f_name);
         return 1;
     }
-
-    // Find file extension separator
-    int sep = 0, cur = 0, len = strlen(f_name);
-    while (cur < len)
-    {
-        if (cur == '.')
-        {
-            sep = cur;
-        }
-        cur++;
-    }
-
-    // Find file extension
-    int ext_len = len - (sep + 1);
-    char extension[ext_len];
-    for (int i = 0; i < ext_len; i++)
-    {
-        extension[i] = f_name[len - ext_len + i];
-    }
-
-    printf("%s", extension);
 }
