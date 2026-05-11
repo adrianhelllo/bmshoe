17927e942056aa08963f551ee48a718222225f4d
adrianhelllo <hetessyadrian@gmail.com>
2026-05-11 21:51:44 +0200

    remove cli flag logic

--- bmshoe.c
+++ bmshoe.c
@@ -25,61 +25,10 @@ char *OPTS = ":?w:h:";
 int main(int argc, char** argv)
 {
     // Ensure correct usage
-    int opt;
-
-    while ((opt = getopt(argc, argv, OPTS)) != -1)
+    if (argc != 2)
     {
-        for (int i = 0; i < OPTARR_NUM; i++)
-        {
-            if (OPTINFO[i].flagopt == opt && OPTINFO[i].has_val == 1)
-            {
-                printf("%s", optarg);
-                if (optarg[0] == '-' && strcmp(optarg, "--") != 0)
-                {
-                    printf("Expected value for -%c.\n", opt);
-                    return 5;
-                }
-            }
-        }
-
-        switch (opt) {
-            // An option is missing its value
-            case ':':
-                printf("Missing value for -%c\n", optopt);
-                return 1;
-
-            // User uses the help option
-            case '?':
-                if (optopt == '?')
-                {
-                    for (int i = 0, len = OPTARR_NUM; i < len; i++)
-                    {
-                        optflag cur_opt = OPTINFO[i];
-
-                        printf("-%c: %s. ", cur_opt.flagopt, cur_opt.description);
-
-                        if (cur_opt.has_val) {printf("Option must take a value.\n");}
-                        else {printf("Option does not take a value.\n");}
-                    }
-                }
-                else
-                {
-                    printf("Unrecognised option. Use the -? option for more information.\n");
-                    return 2;
-                }
-                break;
-
-            case 'w':
-                
-            
-            case 'h':
-                break;
-
-            // Fallback case
-            default:
-                printf("Correct usage: ./bmshoe [opt1, opt2, ..., --] file.bmp. Use the -? option for more information.\n");
-                return 3;
-        }
+        printf("Usage: ./bmshoe image.bmp");
+        return 1;        
     }
         
     // Open .bmp file for reading
@@ -88,6 +37,6 @@ int main(int argc, char** argv)
     if (f == NULL)
     {
         printf("Could not open %s.", f_name);
-        return 4;
+        return 2;
     }
 }
